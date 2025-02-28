ead79fc40f63788f90813372ac4e51c49c18dec8
harisnaseer24 <harisnaseer258@gmail.com>
2025-02-28 18:51:36 +0500

    Fundamentals of Javascript

---
+++ basics/app.js
@@ -0,0 +1,112 @@
+// alert("hi");
+// console.log("hi");
+
+// variables
+// var, let, const
+// var fullName ="Haris Naseer";//string
+// let age=24;//number
+// const isPakistani=true;//boolean
+// isPakistani=false;//error
+
+// let age=35;
+// var fullName="Ali";
+// var fullName="Ali";
+// var fullName="Ali";
+
+// let a;//declaration
+// a=87;//intialization
+// // a=78;
+// //  a=343;
+
+//  let num1=23;
+//  let num2="3fdgfdsgf";
+
+//  //operators
+//     //arithmatic operators + - * / %
+//     console.log(num1+num2);//concatenation 
+//     console.log("Haris"+" "+"Naseer");//concatenation 
+//     console.log(num1-num2);
+//     console.log(num1*num2);
+//     console.log(num1/num2);
+//     console.log(num1%num2);
+    //increment decrement ++ --
+//post increment
+
+// console.log(a++);//a=a+1=>88
+// console.log(a);
+
+// pre-increment
+// console.log(++a);
+// console.log(a);
+// ++a;//a=a+1=>89
+// a--;
+
+// --a;
+
+    //assignment operators = += -= *= /= %=
+// let b=5;
+// b+=4;//b= b+4 => 5+4=>9
+// b-=4;//b= b-4 => 9-4=>5
+// b*=4;//b= b*4 => 5*4=>20
+// b/=4;//b= b+4 => 20/4=>5
+// b%=4;//b= b+4 => 5%4=>1
+// console.log(b);
+    //comparison operators == === != > < >= <= (bool i.e true or false)
+
+// console.log(b=='5');//true
+
+// console.log(b==='5');//false
+// console.log(b !='5');//false
+// console.log(b !=='5');//true
+// console.log(b < 67);//true
+// console.log(b > 4);//true
+// console.log(b >= 4);//true
+// console.log(b <= 4);//false
+
+
+    //logical operators && || !
+
+    // console.log(b<89 && b==8 && b<=45);//false
+
+    // console.log(b<89 ||!(b<89)|| b<=45);//true
+
+    // console.log(!(b<89));//false
+
+    // username||email||phone  && password
+// "haris"                         "sadjhf123" => true
+// "haris@gmail.com"                         "sadjhf123"=>true
+
+
+// Conditional Statements
+// if else
+// if (43759827432798 > 43759827432798  && 487658435!=7326483264) {
+//     console.log("if chal gaya");
+// } else {
+//     console.log("else chal gaya");
+    
+// }
+// nested ifs
+
+// if (7 == 7) {
+//     console.log("if chal gaya");
+//     if (8 !=7) {
+//         console.log("nested if chal gaya");
+//     }else{
+//         console.log("nested else chal gaya");
+//     }
+// } else {
+//     console.log("else chal gaya");
+    
+// }
+// if else ladder
+if (43759827432798 > 43543543543543543545  && 487658435!=7326483264) {
+    console.log("if chal gaya");
+} else if(8-8==0){
+    console.log("else if chal gaya");
+    
+}else{
+    console.log("else chal gaya");
+
+}
+// switch
+// ternary operator
