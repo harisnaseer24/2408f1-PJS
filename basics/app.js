c439ea1a398e5dac409af311236c9db0d08a22de
harisnaseer24 <harisnaseer258@gmail.com>
2025-03-04 16:27:39 +0500

    functions and arrays

--- basics/app.js
+++ basics/app.js
@@ -99,14 +99,170 @@
     
 // }
 // if else ladder
-if (43759827432798 > 43543543543543543545  && 487658435!=7326483264) {
-    console.log("if chal gaya");
-} else if(8-8==0){
-    console.log("else if chal gaya");
+// if (43759827432798 > 43543543543543543545  && 487658435!=7326483264) {
+//     console.log("if chal gaya");
+// } else if(8-8==0){
+//     console.log("else if chal gaya");
     
-}else{
-    console.log("else chal gaya");
+// }else{
+//     console.log("else chal gaya");
 
-}
+// }
 // switch
+
+
+// let number=1;
+// switch (number) {
+//     case 1:
+//         console.log("Monday");
+//         break;
+//      case 2:
+//      console.log("Tuesday");
+//    break;
+//    case 3:
+//     console.log("Wednesday");
+//     break;
+//  case 4:
+//  console.log("Thursday");
+// break;
+//     default:
+//         console.log("I Don't know");
+//         break;
+// }
+
+
+//jump statements(break , continue)
+
 // ternary operator
+
+console.log( 90-5>=23 ?"hi":"bye");
+
+
+//loop (to repeat a task till a condition)
+//for loop (when we know the number of iterations);
+// initialization; condition; increment/decrement
+// i here is a loop variable and iterator and have a local scope;
+
+// for(let i=1;        i <=10000;     i++){
+
+// console.log(i);
+// }
+// 
+// reverse loop
+// table of 7 till 23
+
+
+
+//while loop
+// let i=1;
+// while(i<=10)
+// {
+//     if (i%2!=0) {
+        
+//         console.log(i);
+//         i++;
+//     }else{
+       
+//         i++;//i=8
+//         continue;//skip the odd numbers
+//     }
+   
+//  }
+
+// let i=12;
+// while(i<=10)
+// {
+//    console.log(i);
+//    i++;
+   
+//  }
+
+
+//do while loop
+
+// let i=12;
+// do
+// {
+//    console.log(i);
+//    i++;
+   
+//  }while(i<=10);
+
+
+//functions
+//defining a function
+// function greet(){
+//     console.log("Hello world")
+// }
+
+//invoke / call
+// greet();
+
+//returning function;
+// function greetHi(){
+//     return "Hi";
+// }
+
+
+// let greet=greetHi();//Hi
+// console.log(greet);
+
+//parameters
+// function greetWithName(name){
+//     console.log("Hi "+name);
+// }
+
+// //arguments
+// greetWithName("Haris");
+// greetWithName("Shehzad");
+// greetWithName("Kabeer");
+
+
+// function add(a,b){
+// return a+b;
+
+// }
+
+// let c=add(5,5);//10
+// console.log(c)
+
+// console.log(add(25,5))
+
+//arrow function
+// let addition=(a,b)=>a+b;
+// addition(5,5);
+
+
+//arrrays (collection of elements/values)
+let students=["Ali","Ahmed","Haris","Kabeer","Shehzad"];
+let employees=["Ali","Ahmed","Haris","Kabeer","Shehzad"];
+let Dishes=["Biryani","Qorma","Karahi","Handi","Pulao","Kabab"];
+
+// index = 0-4
+// length = 5
+console.log(students.length)
+console.log(students[3]);
+for(let i=0;i<students.length; i++){
+
+    // document.write("Name => "+students[i] +"<br>");
+    document.write(`Name => ${students[i]} <br>`);//string interpolation
+}
+
+function printArray(arr,heading){
+    for(let i=0; i<arr.length;i++){
+        document.write(`${heading +" "+ (i+1)}  : ${arr[i]}<br>`);
+    }
+}
+
+printArray(students,"Students");
+printArray(employees,"Employees");  
+printArray(Dishes,"Dishes");
+
+
+//objects
+//dom
+//regexp
+//web worker
+//minification
+//es6
+//asychronous programming
