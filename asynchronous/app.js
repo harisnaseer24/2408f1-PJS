9e3104c89dc0e06f7507f67a5c7a6b0fed9d9df3
harisnaseer24 <harisnaseer258@gmail.com>
2025-03-29 16:39:11 +0500

    applying search on api data

--- asynchronous/app.js
+++ asynchronous/app.js
@@ -6,11 +6,43 @@
     .then(
     (data)=>{
         let users=data.users;
-
-        let userList=document.getElementById("userlist")
+        let userList=document.getElementById("userlist");
+        let search=document.getElementById("search");
+        let searchBtn=document.getElementById("searchBtn");
+        searchBtn.addEventListener("click",Search);
 console.log(data.users)
-function Search(){
+function Search(event){
+  event.preventDefault();
 // your search logic here
+let query= search.value.toLowerCase();
+console.log(query)
+let searchResult=users.filter(user=> user.firstName.toLowerCase().includes(query) || user.email.toLowerCase().includes(query) )
+console.log(searchResult)
+if (searchResult.length > 0) {
+  userList.innerHTML =""
+  searchResult.forEach(element => {
+    userList.innerHTML +=
+`<div class="col-lg-4 col-md-6 col-sm-12 my-2">
+    <div class="card">
+        <img src="${element.image}" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">${element.firstName}</h5>
+          <p class="card-text">${element.email} </p>
+          <p class="card-text">${element.phone} </p>
+          <a href="#" class="btn btn-primary">${element.university}</a>
+        </div>
+      </div>
+</div>
+`
+
+
+
+});
+} else {
+  userList.innerHTML="No result found";
+}
+
+
 
 }
 
